1b1884d101d75450ba23dbad22ea3e664f08e281
DATASOLUTION\nrepellin <nrepellin@datasolution.fr>
2023-07-21 17:41:12 +0200

    Product detail, product listing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-      './layout/*.liquid',
-      './sections/*.liquid',
-      './snippets/*.liquid',
-      './templates/customers/*.liquid',
-      './templates/*.liquid'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './layout/*.liquid',
+        './sections/*.liquid',
+        './snippets/*.liquid',
+        './templates/customers/*.liquid',
+        './templates/*.liquid'
+    ],
+    theme: {
+        extend: {
+            height: {
+                '94': '22rem'
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
 
